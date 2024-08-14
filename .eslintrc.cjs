80f2a53514993239685de1c3fedabc080b51c976
Gavin Clive <gavinclive@gmail.com>
2024-08-15 02:23:00 +0700

    chore: update eslint env

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,7 +1,11 @@
 /* eslint-env node */
 module.exports = {
   root: true,
-  'extends': [
+  env: {
+    node: true,
+    browser: true,
+  },
+  extends: [
     'plugin:vue/vue3-essential',
     'eslint:recommended'
   ],
